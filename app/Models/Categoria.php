990710137848c92bc3cc0d4d5741a6ad0e8a8658
steve <stevejph48@gmail.com>
2025-03-07 13:38:14 +0100

    aasd

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'id_user',
+        'visible',
+        'nombre',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }

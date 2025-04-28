11998738a92a6065656e8d0e6ad35a25976d1b50
steve <stevejph48@gmail.com>
2025-04-28 10:24:00 +0200

    nivel

--- resources/views/niveles/show.blade.php
+++ resources/views/niveles/show.blade.php
@@ -4,27 +4,54 @@
     @include('layouts.navheader')
    
     <div class="eventos-main">
-        <div style="display: flex; flex-direction: column; width: 100%; align-items: center;">
+        <div style="display: flex; flex-direction: column; width: 100%; align-items: center; gap: 80px; margin-bottom: 50px;">
 
             <div class="clase-banner">
                 CFGM en Electromecànica de Vehicles Automòbils
-
                 <div>
                     <img src="{{ asset('img/separador.png') }}" alt="Separador">
                 </div>
             </div>
 
-            asdas
+            <div class="clase-descripcion">
+                <div class="clase-descripcion-video">
+                    <iframe 
+                        src="https://www.youtube.com/embed/KFkc5DtcbIM?modestbranding=1&rel=0" 
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>                    
+                </div>
+                <div class="clase-descripcion-texto">
+                    <h2>Vols estudiar el CFGM en Electromecànica de Vehicles Automòbils?
+                    </h2>
+                    <p>
+                    Si t’agrada el món de l’automòbil i vols saber com funciona un motor de combustió, manipular un sistema de suspensió, desmuntar els engranatges i, en definitiva, aprendre tots els secrets de la mecànica, el CFGM en Electromecànica de Vehicles Automòbils és per a tu. Prepara’t per ser un mecànic de nivell capaç de reparar tot tipus d’avaries electromecàniques i treballar en qualsevol taller o concessionari oficial.
+                    </p>
+                    <p>
+                    Cicle LOE
+                    </p>
+                    <p>
+                    Durada: 2000 hores. (2 cursos acadèmics). Horari de tarda.
+                    </p>
+                    <p>
+                    Família professional: Transport i manteniment de vehicles
+                    </p>
+                </div>
+                
+            </div>
 
+
+            <!--
             <div class="video-wrapper">
                 <iframe 
-                    src="https://www.youtube.com/embed/fOdrXsJfEuE" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    src="https://www.youtube.com/embed/fOdrXsJfEuE?modestbranding=1&rel=0"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
                 </iframe>
             </div>
-
+-->
         </div>
     </div>
 </body>

6e8aafd3cbb032bf3c8967ba79f73e669b8ae33d
steve <stevejph48@gmail.com>
2025-04-29 13:36:11 +0200

    qw

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,32 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@include('layouts.head')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+<body>
+    @include('layouts.navheader')
+
+    <div class="perfil-body">
+        <!-- Banner -->
+        <div style="width: 100%; display: flex; flex-direction: column; align-items: center;">    
+            <div class="perfil-banner-container">
+                <div class="perfil-banner">
+                    <img src="../../img/Fondo.png" alt="Banner de perfil">
+                </div>
+
+                <!-- Imagen de perfil y nombre -->
+                <div class="foto-perfil">
+                    <div class="perfil-img">
+                        <img src="../../img/user-icon.png" alt="Foto de perfil">
+                    </div>
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+            <div style="width: 100%; display: flex; flex-direction: column; align-items: center;">
+                <div class="info-perfil">
+                    <h1 class="perfil-nombre">Nombre de Usuario</h1>
+                    <p class="perfil-descripcion">Descripción del usuario</p>
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+            
         </div>
+        
     </div>
-</x-app-layout>
+</body>
